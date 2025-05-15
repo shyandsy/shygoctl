d1e8c5f4ea1fea18f1a1c296d9c6cde3b4b09d26
shyandsy <shyandsy@gmail.com>
2025-05-15 22:34:32 +0800

    clean-folder

--- core/configcenter/subscriber/subscriber.go
+++
@@ -1,9 +0,0 @@
-package subscriber
-
-// Subscriber is the interface for configcenter subscribers.
-type Subscriber interface {
-	// AddListener adds a listener to the subscriber.
-	AddListener(listener func()) error
-	// Value returns the value of the subscriber.
-	Value() (string, error)
-}
